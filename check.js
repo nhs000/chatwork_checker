83a49c2398911dfa9e702a688a74d731d3b84e1d
Nguyen Huu Son <hs@Nguyens-MacBook-Pro.local>
2017-11-23 16:47:11 +0900

    add validation for bracket

--- check.js
+++ check.js
@@ -7,9 +7,39 @@ var checkFn = function(event){
         return event.keyCode == 13 && event.shiftKey;
     }
 }
+
+var cf = function(value){
+    var val_rs = validation(value);
+    var cf_str = "";
+    if(val_rs.status){
+        cf_str = "Sure for submitting?";
+    }else{
+        cf_str = val_rs.log + "Sure for submitting?";
+    }
+    if(confirm(cf_str)){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+var validation = function(value){
+    var open_bracket_stack = [];
+    var close_bracket_stack = [];
+    Array.prototype.forEach.call(value, function(c) {
+        if(c == '[') open_bracket_stack.push(c);
+        if(c == ']') close_bracket_stack.push(c);
+    });
+    if (open_bracket_stack.length != close_bracket_stack.length){
+        return {status: false, log: "Bracket not match. "};
+    }else{
+        return {status: true, log: ""};
+    }
+}
+
 inputElem.addEventListener('keydown', function(event){
     if(checkFn(event)){
-        if(!confirm("Sure for submit?")){
+        if(cf(inputElem.value)){
             event.preventDefault();
         }
     }
